f82af735273e44477f3018ce917ca60e033bbd33
weiyiming <weiym0206@gmail.com>
2013-06-17 13:03:49 +0800

    add help servlet

---
+++ src/com/lionnet/gpay/servlet/Help.java
@@ -0,0 +1,43 @@
+package com.lionnet.gpay.servlet;
+
+import com.lionnet.gpay.core.ProcessHandler;
+import com.lionnet.gpay.core.ProcessHandlerMode;
+import com.lionnet.gpay.utils.Contants;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class DetailQuery
+ */
+public class Help extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Help() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ProcessHandler handler = new ProcessHandler(request, response);
+        handler.setMode(ProcessHandlerMode.WRITE_MODE);
+        handler.pushToUser(Contants.HELP_TEXT);
+    }
+
+}
